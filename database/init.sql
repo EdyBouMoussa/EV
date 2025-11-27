87eee111bb376a010a1398f26ea28144a4bf6077
Edy Bou Moussa <boumoussaedy01@gmail.com>
2025-11-27 18:09:11 +0200

    Removed setup

--- database/init.sql
+++ database/init.sql
@@ -3,8 +3,3 @@ CREATE DATABASE IF NOT EXISTS ev_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicod
 CREATE USER IF NOT EXISTS 'ev_user'@'%' IDENTIFIED BY 'ev_password';
 GRANT ALL PRIVILEGES ON ev_db.* TO 'ev_user'@'%';
 FLUSH PRIVILEGES;
-
-
-
-
-
